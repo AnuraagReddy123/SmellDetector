20d83f7aa619550d5887cc2034b29e407b1acb6c
P. Anuraag Reddy <anuraagreddy74@outlook.com>
2022-03-23 14:43:54 +0530

    Basic button for smell usage

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 define(['base/js/namespace', 'base/js/events'], function (Jupyter, events) {
-  var defaultCellButton = function () {};
+  // This function will detect the smells
+  var findSmells = function () {
+
+    
+    // Replace get_selected_cell with get_all_cell if necessary
+    let cell = Jupyter.notebook.get_selected_cell();
+    let text = cell.get_text();
+    console.log(text);
+
+    // Enter code to find different smells
+
+    // Long Parameter List
+
+    // Long methods
+
+    // Wild Card Imports
+  };
+
+  // Clickable button in toolbar
+  var detectSmellButton = function () {
+    Jupyter.toolbar.add_buttons_group([
+      Jupyter.keyboard_manager.actions.register(
+        {
+          help: 'Detect smells',
+          icon: 'fa-play-circle',
+          handler: findSmells,
+        },
+        'detect-code-smell',
+        'Smell Detector'
+      ),
+    ]);
+  };
 
   function load_ipython_extension() {
-    defaultCellButton();
+    detectSmellButton();
   }
   return {
     load_ipython_extension: load_ipython_extension,
